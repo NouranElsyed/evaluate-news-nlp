7e2bfce01be6fb4480622af2d0445f5beea5cb9e
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-12-04 22:51:45 +0200

    edit the path

--- starter_project/src/server/index.js
+++ starter_project/src/server/index.js
@@ -28,7 +28,9 @@ app.use(bodyParser.json());
 app.use(express.static(distPath));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(distPath, 'index.html'));
+  // res.sendFile(path.join(distPath, 'index.html'));
+  res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
+
 });
 
 
