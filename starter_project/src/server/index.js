bf7d5f7918f749687b4596770c4dad129bd5c8a9
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-12-04 13:27:47 +0200

    Modifydistpath

--- starter_project/src/server/index.js
+++ starter_project/src/server/index.js
@@ -6,8 +6,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
 
-const __filename = path.resolve();
+
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
@@ -16,15 +17,18 @@ const app = express();
 
 console.log(__dirname, __filename);
 
+const distPath = path.resolve(__dirname, 'dist');
+console.log('Dist Path => ', distPath);
+
 const API_URL = 'https://api.meaningcloud.com/sentiment-2.1';
 const API_KEY = process.env.API_KEY;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../../dist')));
+app.use(express.static(distPath));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..','..', 'dist', 'index.html'));
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 
